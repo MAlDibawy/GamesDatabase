a31c9c273f69cf1b8e00da30a4b933b889fbbdf7
MAlDibawy <mahmoudaldibawy@gmail.com>
2023-04-10 17:10:40 +0200

    fixing game component style

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -4,8 +4,8 @@ import GameModal from "./../Modal/GameModal";
 export default function Game({ gId, thumb, title, desc }) {
   return (
     <GameModal gId={gId}>
-      <div>
-        <div className="gameContent bg-secondary p-2 rounded">
+      <div className="h-100">
+        <div className="gameContent bg-secondary p-2 rounded h-100">
           <img src={thumb} alt="test" className="w-100" />
           <h4 className="py-2">{title}</h4>
           <p>{desc}</p>
